a8eb654e98aaffe60d76d87e12526f9fa101d273
Maxwell Lay <maxwell.lay@data61.csiro.au>
2025-09-03 14:44:20 +1000

    set up distill student config

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/lift_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/lift_env_cfg.py
@@ -143,7 +143,7 @@ class ObservationsCfg:
     )
         def __post_init__(self):
             self.enable_corruption = True
-            self.concatenate_terms = True  # keep as (C,H,W) for CNN
+            self.concatenate_terms = True  
 
     # observation groups
     policy: PolicyCfg = PolicyCfg()
