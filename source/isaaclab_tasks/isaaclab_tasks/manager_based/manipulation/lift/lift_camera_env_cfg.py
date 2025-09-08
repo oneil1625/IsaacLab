8fc1b09e521763ae0179136818a6438870a6fe87
Maxwell Lay <maxwell.lay@data61.csiro.au>
2025-09-08 22:51:10 +1000

    concatenate terms

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/lift_camera_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/lift_camera_env_cfg.py
@@ -128,7 +128,7 @@ class ObservationsCfg:
         
         def __post_init__(self):
             self.enable_corruption = True
-            self.concatenate_terms = False
+            self.concatenate_terms = True
     
 
 
