4709b9c931d1ea495f04ac725870cf02f068ac8b
Maxwell Lay <maxwell.lay@data61.csiro.au>
2025-09-08 21:44:10 +1000

    add distill student yaml entry point

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/config/franka/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/config/franka/__init__.py
@@ -54,6 +54,7 @@ gym.register(
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
+        "distill_cfg_entry_point": f"{agent.__name__}:distill_student.yaml"
     },
     disable_env_checker=True,
 )
