fec4aa868d92acd5da833e1878a40cefc0a192bf
Maxwell Lay <maxwell.lay@data61.csiro.au>
2025-09-12 15:25:36 +1000

    comment out logger

--- scripts/reinforcement_learning/rsl_rl/cli_args.py
+++ scripts/reinforcement_learning/rsl_rl/cli_args.py
@@ -81,8 +81,8 @@ def update_rsl_rl_cfg(agent_cfg: RslRlOnPolicyRunnerCfg, args_cli: argparse.Name
         agent_cfg.load_checkpoint = args_cli.checkpoint
     if args_cli.run_name is not None:
         agent_cfg.run_name = args_cli.run_name
-    if args_cli.logger is not None:
-        agent_cfg.logger = args_cli.logger
+    # if args_cli.logger is not None:
+    #     agent_cfg.logger = args_cli.logger
     # set the project name for wandb and neptune
     if agent_cfg.logger in {"wandb", "neptune"} and args_cli.log_project_name:
         agent_cfg.wandb_project = args_cli.log_project_name
