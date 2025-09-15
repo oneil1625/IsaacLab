bfb9d96792cd650b4037b2ff643037a0d5054bd9
Maxwell Lay <maxwell.lay@data61.csiro.au>
2025-09-15 22:24:27 +1000

    add disill config

---
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/config/franka/agents/rsl_rl_distill_cfg.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+from isaaclab.utils import configclass
+from isaaclab_rl.rsl_rl import (
+    RslRLOnPolicyRunnerCfg,
+)
+from isaaclab_rl.rsl_rl.distillation_cfg import (
+    RslRlDistillationStudentTeacherCfg,
+    RslRlDistillationAlgorithmCfg,
+)
+
+@configclass
+class DistillStudentRunnerCfg(RslRLOnPolicyRunnerCfg):
+    num_steps_per_env = 24
+    max_iterations = 1500
+    save_interval = 50
+    experiment_name = "distill_student"
+    empirical_normalization = False
+
+    policy = RslRlDistillationStudentTeacherCfg(
+        init_noise_std=1.0,
+        noise_std_type="scalar",
+        student_hidden_dims=[256, 128, 64],
+        teacher_hidden_dims=[512, 256, 128],
+        activation="elu"
+    )
+
+    algorithm = RslRlDistillationAlgorithmCfg(
+        num_learning_epochs=5,
+        learning_rate=1e-4,
+        gradient_length=48,
+        max_grad_norm=1.0,
+    )
+
+    load_run = "2025-08-06_14-57-09"
+    load_checkpoint = "model_2999.pt"
